e0343b33f40eb236201a20db002cf32935f5f63a
Nacht <nachtzhong@tungee.com>
2023-05-30 22:52:05 +0800

    initial commit

---
+++ src/com.nacht/design_pattern/HelloWorld.java
@@ -0,0 +1,11 @@
+package design_pattern;
+
+/**
+ * @author Nacht
+ * Created on 2023/5/30 22:50
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
